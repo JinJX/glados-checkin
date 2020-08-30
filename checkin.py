c6440e60da6299b2be3206d296a8ca4fd0b952a3
JinJX <57978718@qq.com>
2020-08-30 09:21:15 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -5,7 +5,7 @@ sever = '0'
 # 填写server酱sckey,不开启server酱则不用填
 sckey = 'SCU89402Tf98b7f01ca3394b9ce9aa5e2ed1abbae5e6ca42999999'
 # 填入glados账号对应cookie
-cookie = '__cfduid=d768ca8690328b389b5553631d75e93481594649930; koa:sess=eyJ1c2VySWQiOjQzNjAyLCJfZXhwaXJlIjoxNjIwNTY5OTcwMTM4LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=slAtjHvFh2bKBbbu_9igpXRkB60'
+cookie = '__cfduid=db79479eaddb227c9ee91b229b62d896b1598747991; _ga=GA1.2.2047428545.1598747984; _gid=GA1.2.1075437917.1598747984; koa:sess=eyJ1c2VySWQiOjQ3NzEzLCJfZXhwaXJlIjoxNjI0NjY4MTY3ODM2LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=Rd8M0wv_tYTfniG0MUVw97I5pPE'
 referer = 'https://glados.rocks/console/checkin'
 
 def start():
